2654672960be5af6f4d312006c26015382fd9b70
Aniket Pradhan <aniketpradhan814@gmail.com>
2023-03-25 09:19:07 +0530

    Footer css added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <h4>Footer</h4>
-    </div>
+    <footer>
+      <p>&copy; 2023 KitchenKorner. All Rights Reserved.</p>
+    </footer>
   );
 };
 
