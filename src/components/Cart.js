e09c1b7d9efc3003b5e2a645240c2a9ff85533c8
Aniket Pradhan <aniketpradhan814@gmail.com>
2023-03-31 15:19:34 +0530

    Update Cart component CSS and added new feature

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { IMG_CDN_URL } from "../config";
 import { useSelector, useDispatch } from "react-redux";
-import { clearCart } from "../utils/redux/slices/cartSlice";
+import { clearCart, removeItemFromCart } from "../utils/redux/slices/cartSlice";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   const dispatch = useDispatch();
@@ -10,25 +12,146 @@ const Cart = () => {
     dispatch(clearCart());
   };
 
-  return (
-    <div>
-      <h1>Cart Items</h1>
-      <h2>Cart Items length = {items.length}</h2>
-      {items.length > 0 ? (
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => handleClearCart()}
-        >
-          Clear Cart
-        </button>
-      ) : null}
-      {items.map((item) => (
-        <div key={item.id}>
-          <p>{item.name}</p>
-          <p>{item.price/100}</p>
+  const itemQuantityMap = items.reduce((accumulator, item) => {
+    accumulator[item.name] = accumulator[item.name] || { ...item, quantity: 0 };
+    accumulator[item.name].quantity++;
+    return accumulator;
+  }, {});
+
+  const totalPrice = items.reduce((accumulator, item) => {
+    return accumulator + item.price;
+  }, 0);
+
+  const removeItem = (id) => {
+    console.log(id)
+    dispatch(removeItemFromCart(id));
+  };
+
+  if (items.length === 0) {
+    return (
+      <>
+        <div className="flex flex-col items-center justify-center h-screen">
+          <img
+            src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_200/2xempty_cart_yfxml0"
+            alt="Empty Cart"
+            className="mb-8"
+          />
+          <p className="mb-4 text-lg font-semibold">Your cart is empty</p>
+          <button className="px-4 py-2 text-white bg-orange-500 rounded hover:bg-orange-600 focus:outline-none">
+            <Link to="/">See restaurants near you</Link>
+          </button>
         </div>
-      ))}
-    </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div style={{ background: "#E9ECEE" }}>
+        <div className="mx-auto max-w-2xl px-4 pt-16 pb-24 sm:px-6 lg:max-w-7xl lg:px-8">
+          <form className="mt-12 lg:grid lg:grid-cols-12 lg:items-start lg:gap-x-12 xl:gap-x-16">
+            <section
+              aria-labelledby="cart-heading"
+              className="lg:col-span-8 bg-white text-black"
+            >
+              <h2 id="cart-heading" className="sr-only">
+                Items in your shopping cart
+              </h2>
+
+              <ul
+                role="list"
+                className="divide-y divide-gray-200 border-t border-b border-gray-200"
+              >
+                {Object.values(itemQuantityMap).map((item) => (
+                  <div key={item.id} className="px-4">
+                    <li className="flex py-6 sm:py-6 ">
+                      <div className="flex-shrink-0">
+                        <img
+                          className="h-24 w-24 rounded-md object-contain object-center sm:h-38 sm:w-38"
+                          src={`${IMG_CDN_URL}${item.imageId}`}
+                          alt={item.name}
+                        />
+                      </div>
+
+                      <div className="ml-4 flex flex-1 flex-col justify-between sm:ml-6">
+                        <div className="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
+                          <div>
+                            <div className="flex justify-between">
+                              <h3 className="font-medium text-lg text-black">
+                                {item.name} [&#x2715; {item.quantity}]
+                              </h3>
+                            </div>
+                            <div className="mt-1 flex items-end">
+                              <p className="text-sm font-medium text-black">
+                                ₹ {item.price / 100}
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <p onClick={() => removeItem(item.id)}>Remove</p>
+                    </li>
+                  </div>
+                ))}
+              </ul>
+            </section>
+
+            {/* Order summary */}
+            <section
+              aria-labelledby="summary-heading"
+              className="mt-16 rounded-md bg-white lg:col-span-4 lg:mt-0 lg:p-0"
+            >
+              <h2
+                id="summary-heading"
+                className=" px-4 py-3 sm:p-4 border-b border-gray-200 text-lg font-medium"
+              >
+                Price Details
+              </h2>
+
+              <div>
+                <dl className=" space-y-1  px-6 py-4 sm:p-6">
+                  <div className="flex items-center justify-between">
+                    <dt className="text-sm">Item Total</dt>
+                    <dd className="text-sm font-medium">
+                      ₹ {totalPrice / 100}
+                    </dd>
+                  </div>
+                  <div className="flex items-center justify-between py-4">
+                    <dt className="text-sm">Govt Taxes & Other Charges</dt>
+                    <dd className="text-sm font-medium">
+                      ₹ {((totalPrice / 100) * 0.05).toFixed(2)}
+                    </dd>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <dt className="flex text-sm">
+                      <span>Delivery Charges</span>
+                    </dt>
+                    <dd className="text-sm font-medium text-green-700 dark:text-green-400">
+                      Free
+                    </dd>
+                  </div>
+                  <div className="flex items-center justify-between py-4 border-y border-dashed ">
+                    <dt className="text-base font-medium">Total Amount</dt>
+                    <dd className="text-base font-medium">
+                      ₹{" "}
+                      {(
+                        Number(totalPrice / 100) +
+                        Number(((totalPrice / 100) * 0.05).toFixed(2))
+                      ).toFixed(2)}
+                    </dd>
+                  </div>
+                </dl>
+                <div className="px-6 pb-4">
+                  <button className="bg-green-400 w-full text-white p-3 font-bold">
+                    Checkout
+                  </button>
+                </div>
+              </div>
+            </section>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
